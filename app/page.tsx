8cfd9fc84254e4f93d6ed6bb067600f22b842263
NJ13 <nozomu0715n@gmail.com>
2026-05-01 04:48:20 +0900

    fix import path

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Concept from "@/components/Concept";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import Menu from "@/components/Menu";
-import ShopInfo from "@/components/ShopInfo";
+import Concept from "../components/Concept";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Menu from "../components/Menu";
+import ShopInfo from "../components/ShopInfo";
 
 export default function Home() {
   return (
